dcd84b58533843185d651d1428786de5e34ca018
Sebastian Polsterl <sebp@src.gnome.org>
2007-10-16 17:37:49 +0000

    Added changelog element to XML schema
    svn path=/trunk/; revision=40

--- src/libcapuchin/Repository.cs
+++ src/libcapuchin/Repository.cs
@@ -53,137 +53,95 @@ namespace Capuchin
 
     [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
     public class item
-    {
-
-        private author authorField;
-        private checksum checksumField;
-        private string descriptionField;
-        private string idField;
-        private string nameField;
-        private string signatureField;
-        private string[] tagsField;
-        private string urlField;
-        private string versionField;
-        
+    {   
         [System.Xml.Serialization.XmlElementAttribute("author", typeof(author), IsNullable = false)]
-        public author Author
-        {
-            get
-            {
-                return this.authorField;
-            }
-            set
-            {
-                this.authorField = value;
-            }
-        }
+        public author Author;
+		
         [System.Xml.Serialization.XmlElementAttribute("checksum", typeof(checksum), IsNullable = false)]
-        public checksum Checksum
-        {
-            get
-            {
-                return this.checksumField;
-            }
-            set
-            {
-                this.checksumField = value;
-            }
-        }
+        public checksum Checksum;
+		
         [System.Xml.Serialization.XmlElementAttribute("description", typeof(string), IsNullable = false)]
-        public string Description
-        {
-            get
-            {
-                return this.descriptionField;
-            }
-            set
-            {
-                this.descriptionField = value;
-            }
-        }
+        public string Description;
+		
         [System.Xml.Serialization.XmlElementAttribute("id", typeof(string))]
-        public string Id
-        {
-            get
-            {
-                return this.idField;
-            }
-            set
-            {
-                this.idField = value;
-            }
-        }
+        public string Id;
+		
         [System.Xml.Serialization.XmlElementAttribute("name", typeof(string))]
-        public string Name
-        {
-            get
-            {
-                return this.nameField;
-            }
-            set
-            {
-                this.nameField = value;
-            }
-        }
+        public string Name;
+		
         [System.Xml.Serialization.XmlElementAttribute("signature", typeof(string), DataType = "anyURI", IsNullable = false)]
-        public string Signature
-        {
-            get
-            {
-                return this.signatureField;
-            }
-            set
-            {
-                this.signatureField = value;
-            }
-        }
-        
+        public string Signature;
+		
         [XmlArrayAttribute("tags", IsNullable=false)]
         [XmlArrayItemAttribute("tag", typeof(string), IsNullable=false)]
-        public string[] Tags
-        {
-            get
-            {
-                return this.tagsField;
-            }
-            set
-            {
-                this.tagsField = value;
-            }
-        }
+        public string[] Tags;
         
         [System.Xml.Serialization.XmlElementAttribute("url", typeof(string), DataType = "anyURI")]
-        public string Url
-        {
-            get
-            {
-                return this.urlField;
-            }
-            set
-            {
-                this.urlField = value;
-            }
-        }
+        public string Url;
+		
         [System.Xml.Serialization.XmlElementAttribute("version", typeof(string))]
-        public string Version
+        public string Version;
+		
+		[System.Xml.Serialization.XmlElementAttribute("changelog", typeof(changelog), IsNullable = false)]
+		public changelog Changelog;		
+    }
+	
+	[XmlRootAttribute(ElementName="changelog", Namespace = "", IsNullable = false)]
+    public class changelog : Dictionary<string, string>, IXmlSerializable
+    {
+        public System.Xml.Schema.XmlSchema GetSchema()
         {
-            get
-            {
-                return this.versionField;
-            }
-            set
-            {
-                this.versionField = value;
-            }
+            return null;
+        }
+        
+        public void ReadXml(XmlReader reader)
+		{
+			reader.Read();
+			
+			string version = null;
+			string text = null;
+			while (reader.Name != "changelog")
+			{
+				switch (reader.NodeType)
+				{
+					case XmlNodeType.Element:
+						reader.MoveToAttribute("version");
+						version = reader.Value;
+					break;
+					case XmlNodeType.Text:
+					    text = reader.Value;
+					break;
+				    case XmlNodeType.EndElement:
+					    if (version != null && text != null)
+					    {
+						     this.Add(version, text);
+						     version = text = null;
+					    }
+					break;
+				}
+				reader.Read();
+			}
+			
+			reader.ReadEndElement();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+			foreach ( string key in this.Keys )
+			{
+				writer.WriteStartElement("changes");
+				writer.WriteAttributeString("version", key);
+				writer.WriteEndElement();
+			}
         }
     }
-
+    
+	
     [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
     public class checksum
     {
 
         private checksumType typeField;
-
         private string textField;
 
         [System.Xml.Serialization.XmlAttributeAttribute()]
